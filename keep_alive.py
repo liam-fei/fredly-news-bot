1305141fc0ec7f7ab390a69f074113da85cc85ca
liam-fei <linhangd12@gmail.com>
2025-11-02 01:37:24 +0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-
 # keep_alive.py
 from flask import Flask
 from threading import Thread
@@ -11,8 +10,8 @@ def home():
     return "Fredly News Bot is ALIVE! (Render 24/7)", 200
 
 def run():
-    # 强制使用 Render 提供的 PORT，不能 fallback
-    port = int(os.environ.get("PORT"))
+    # 强制使用 Render 提供的 PORT，**去掉 8080！**
+    port = int(os.environ["PORT"])  # 不能用 get(..., 8080)
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
